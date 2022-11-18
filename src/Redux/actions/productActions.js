a5ea3d03330fd05d8513ef642bf7d3dd7750a2bf
Ashish0000777 <Ashishkhadse0p@gmail.com>
2022-11-18 10:32:01 +0530

    flipkart frontend

---
+++ src/Redux/actions/productActions.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import * as actionType from "../constants/ProductConstance";
+
+export const getproduct = () => async (dispatch) => {
+  try {
+    let { data } = await axios.get(`https://flipcartserverdb.herokuapp.com/products`);
+    return dispatch({ type: actionType.GET_PRODUCTS_SUCCESS, payload: data });
+  } catch (error) {
+    return dispatch({
+      type: actionType.GET_PRODUCTS_FAILED,
+      payload: error.message,
+    });
+  }
+};
+
+export const getProductById = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: actionType.GET_PRODUCT_BY_ID_REQUESTED });
+    let { data } = await axios.get(`https://flipcartserverdb.herokuapp.com/product/${id}`);
+    dispatch({ type: actionType.GET_PRODUCT_BY_ID_SUCCESSFULL, payload: data });
+  } catch (error) {
+    dispatch({
+      type: actionType.GET_PRODUCT_BY_ID_FAILED,
+      payload: error.message,
+    });
+  }
+};
---
+++ src/Services/Api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+export const AuthenticateSignup = async (data) => {
+  try {
+    const url = "https://flipcartserverdb.herokuapp.com/signup";
+    return await axios.post(url,data);
+  } catch (error) {
+    console.log("error while calling signup api", error);
+  }
+};
+
+export const AuthenticateLogin = async (Data) => {
+    try {
+      const url = "https://flipcartserverdb.herokuapp.com/login";
+      return await axios.post(url,Data);
+    } catch (error) {
+      console.log("error while calling login api", error);
+      return error.responce
+    }
+  };
